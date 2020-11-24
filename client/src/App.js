e79be34c7978db9ed7d86222070f6c747c251855
Elwin Ung <elwin.ung@gmail.com>
2020-11-24 11:16:19 -0500

    Added global context

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
+import globalContext from './utils/globalContext';
 
 function App() {
+
+  const [data, setData] = useState({
+    select: '',
+    mode: '',
+    questions: [],
+    setValue: (name, value) =>{
+        setData({...data, [name]: value});
+    }
+  });
+
   return (
+    <globalContext.Provider value={data}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
+    </globalContext.Provider>
   );
 }
 
