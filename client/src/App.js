42a3b1a90805cb8f87f897fd4d42f9f0e7c7dae9
Cecil <69444825+cek333@users.noreply.github.com>
2020-11-25 11:50:32 -0500

    added login/signup to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,95 @@
+import React, { useState } from "react";
 import './App.css';
-import {BrowserRouter as Router, Route} from "react-router-dom"
-import NavBar from "./components/NavBar"
-import Home from "./components/Home"
+import {BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import LoginSignUp from "./components/LoginSignUp";
 
-function App() {  
+function App() {
+  // DEBUG API (until real API added)
+  const API = {
+    createUser: function(data) {
+      console.log('[createUser]', data);
+    },
+    loginUser: function(data) {
+      console.log('[loginUser]', data);
+      return Math.random()<0.8 ? {success: true} : {success: false};
+    }
+  }
+  // LOGIN FORM STATE AND FUNCTIONS
+  const [curUser, setCurUser] = useState('');
+  const [formInfo, setFormInfo] = useState({ username:'', password:'', email:'', error:'' });
+  const [formAction, setFormAction] = useState('Login');
+  const [displayLogin, setDisplayLogin] = useState(false);
+
+  function handleLoginChange(evt) {
+    let { name, value } = evt.target;
+    // Update changed field + clear any error messages
+    setFormInfo({...formInfo, [name]: value, error: ''});
+  }
+
+  function handleLoginSubmit(evt) {
+    evt.preventDefault();
+    // clear error message
+    console.log('[handleSubmit]', formInfo);
+    // check for valid email address
+    if (formAction==='SignUp' &&
+      !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(formInfo.email)) {
+      setFormInfo({...formInfo, error: 'Invalid email address!'});
+      return;
+    }
+    if (formAction==='SignUp') {
+      API.createUser({
+        username: formInfo.username,
+        email: formInfo.email,
+        password: formInfo.password
+      });
+    } else {
+      let res = API.loginUser({
+        username: formInfo.username,
+        password: formInfo.password
+      });
+      // Check success
+      if (!res.success) {
+        setFormInfo({...formInfo, error: 'Invalid credentials!'});
+        return;
+      }
+    }
+    // Successful login/signup!
+    setCurUser(formInfo.username);
+    // Clear form fields
+    setFormInfo({ username:'', password:'', email:'', error:'' });
+    // Hide form box
+    setDisplayLogin(false);
+  }
+
+  function handleLoginFormType() {
+    console.log('[handleLoginFormType]');
+    if (formAction==='Login') {
+      setFormAction('SignUp');
+    } else {
+      setFormAction('Login');
+    }
+  }
+
+  let loginButton;
+  if (curUser) {
+    loginButton = <button className="mx-2" onClick={()=>{setCurUser(''); setFormAction('Login');}}>Logout</button>;
+  } else {
+    loginButton = <button className="mx-2" onClick={()=>setDisplayLogin(true)}>Login/SignUp</button>
+  }
+
   return(
-  <div className="App">
-  <NavBar/>
-  <Home />
-
-  </div>
+  <Router>
+    <div className="App">
+      <NavBar/>
+      <Route path="/login">
+        <LoginSignUp action={formAction} handleClick={handleLoginFormType}
+                    formInfo={formInfo} handleChange={handleLoginChange} handleSubmit={handleLoginSubmit} />
+      </Route>
+      <Home />
+    </div>
+  </Router>
 );
 }
 
---
+++ client/src/components/LoginSignUp/index.js
@@ -0,0 +1,48 @@
+import './LoginSignUp.css';
+import { useHistory } from 'react-router-dom';
+
+function LoginSignUp(props) {
+  let history = useHistory();
+  let disableSubmit = !(props.formInfo.username && 
+                        props.formInfo.password && (props.formInfo.password.length>=8) && 
+                        (props.action==='SignUp' ? props.formInfo.email : true)); 
+
+                        // Allow user to switch between login/signup
+  let switchOption, switchText, switchButtonText;
+  if (props.action==='Login') {
+    switchText = "New User? Click here to";
+    switchButtonText = "SignUp";
+  } else {
+    switchText = "Already Registered? Click here to";
+    switchButtonText = "Login";
+  }    
+  switchOption = <p className="mb-2 text-center">{switchText}
+    <span className="font-weight-bold" style={{cursor: 'pointer'}} 
+          onClick={props.handleClick}> {switchButtonText}</span></p>;
+
+  return (
+    <div className="bg-secondary p-2 text-white login-signup">
+      <form className="d-flex flex-column" onSubmit={props.handleSubmit}>
+        {props.action==='SignUp' &&
+          <>
+            <label htmlFor="email">email</label>
+            <input className="mb-3" type="text" id="email" name="email" placeholder="email"
+                   value={props.formInfo.email} onChange={props.handleChange} />
+          </>
+        }
+        <label htmlFor="username">username</label>
+        <input className="mb-3" type="text" id="username" name="username" placeholder="username"
+               value={props.formInfo.username} onChange={props.handleChange} />
+        <label htmlFor="password">password (8+ characters) (currently:{props.formInfo.password.length})</label>
+        <input className="mb-3" type="password" id="password" name="password" placeholder="password"
+               value={props.formInfo.password} onChange={props.handleChange} />
+        <button className="mb-2" type="submit" disabled={disableSubmit}>{props.action}</button>
+        <button className="mb-2" type="button" onClick={()=>history.goBack()}>Cancel</button>
+        <p className="text-center font-weight-bold">{props.formInfo.error}</p>
+        {switchOption}
+      </form>
+    </div>
+  );
+}
+
+export default LoginSignUp;
