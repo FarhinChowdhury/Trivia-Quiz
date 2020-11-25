5a0695a666f2f4a6636f9d8b5067c9e813b06cdb
Cecil <69444825+cek333@users.noreply.github.com>
2020-11-25 13:41:22 -0500

    navigate to home page after login/signup (for now)

--- client/src/components/LoginSignUp/index.js
+++ client/src/components/LoginSignUp/index.js
@@ -7,7 +7,7 @@ function LoginSignUp(props) {
                         props.formInfo.password && (props.formInfo.password.length>=8) && 
                         (props.action==='SignUp' ? props.formInfo.email : true)); 
 
-                        // Allow user to switch between login/signup
+  // Allow user to switch between login/signup
   let switchOption, switchText, switchButtonText;
   if (props.action==='Login') {
     switchText = "New User? Click here to";
@@ -20,9 +20,19 @@ function LoginSignUp(props) {
     <span className="font-weight-bold" style={{cursor: 'pointer'}} 
           onClick={props.handleClick}> {switchButtonText}</span></p>;
 
+  function submitThenNavigate(evt) {
+    // console.log('[submitThenNavigate]');
+    if (props.handleSubmit(evt)) {
+      // console.log('[submitThenNavigate] (User logged in/Signed up)');
+      // Redirect to Game page (TBD)
+      // Redirect to Home page for now
+      history.push("/");
+    }
+  }
+
   return (
     <div className="bg-secondary p-2 text-white login-signup">
-      <form className="d-flex flex-column" onSubmit={props.handleSubmit}>
+      <form className="d-flex flex-column" onSubmit={submitThenNavigate}>
         {props.action==='SignUp' &&
           <>
             <label htmlFor="email">email</label>
