f5ea2030d12215934d357811ea8a1dc4a486a86e
Garman_K <garnetbirthstone@hotmail.com>
2020-11-26 13:38:29 -0500

    switched to ORM function names

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,14 +1,26 @@
 const router = require("express").Router();
+const { 
+    createUser,
+    loginUser,
+    getScoreTA,
+    getScoreLVL,
+    updateScoreTA,
+    updateScoreLVL
+} = require("../../config/orm");
+
 const userController = require("../../controllers/userController");
 
-router.get('/highscore/ta', userController.findAll);
+router.get('/highscore/ta', getScoreTA.findAll);
 
-router.get('/highscore/lvl', userController.findAll);
+router.get('/highscore/lvl', getScoreLVL.findAll);
 
-router.post('/user', userController.create);
+router.post('/user', createUser.create);
 
-router.post('/user/auth', userController.check);
+router.post('/user/auth', loginUser.check);
 
-router.put('/user/:id', userController.findOneAndUpdate);
+router.put('/user/:id', updateScoreTA.findOneAndUpdate);
+
+router.put('/user/:id', updateScoreLVL.findOneAndUpdate);
+
 
 module.exports = router;
