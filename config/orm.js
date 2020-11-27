038aebca09df968f1ccd675a035a222ac9197ac0
Elwin Ung <elwin.ung@gmail.com>
2020-11-27 11:41:55 -0500

    Added bycrypt and added password hashing in ORM

--- config/orm.js
+++ config/orm.js
@@ -1,19 +1,38 @@
 const userData = require('../models/users.js');
 const path = require('path');
+const bcrypt = require( 'bcrypt' );
+
+async function passwordHash(password){
+    const hashedPassword = await bcrypt.hash(password.trim(), 10);
+    return hashedPassword;
+}
+
+async function passwordCompare(password, hashedPassword){
+    const isValidPassword = await bycrypt.compare(password.trim(), hashedPassword);
+    return isValidPassword;
+}
+
 
 let orm = {
 
     createUser: function (req, res) {
+        req.body.data.password = passwordHash(req.body.data.password);
         userData
-        .create(req.body)
-        .then(userData => res.send(userData))
+        .create(req.body.data)
+        .then(data => res.send(data))
         .catch(err => console.log(err));
     },
 
     loginUser: function (req, res) {
         userData
         .findOne ({username: req.body.username})
-        .then (data => res.send(data))
+        .then ((data) => {
+            if(passwordCompare(req.body.password, data.password)) res.send(data);
+            else {
+                alert('Incorrect username/password');
+                return false;
+            }
+        })
         .catch(err => res.status(422).json(err));
     },
 
@@ -29,6 +48,13 @@ let orm = {
         userData
         .findOne({username: req.body.username})
         .sort({highscore_LVL: -1})
+        .then (data => res.json(data))
+        .catch(err => res.status(422).json(err));
+    },
+
+    updateHighscore: function (req, res){
+        userData
+        .findOneAndUpdate({_id: req.params.id}, req.body.data)
         .then (data => res.json(data))
         .catch(err => res.status(422).json(err));
     },
